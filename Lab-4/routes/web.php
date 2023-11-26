32b538fa6979328887f2d98e3aed4e50c97f5ef2
AndriyVohar <vohar.andrii@student.uzhnu.edu.ua>
2023-11-26 17:55:28 +0200

    Npm init

--- Lab-4/routes/web.php
+++ Lab-4/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TournamentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,18 +14,32 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//
-Route::get('/tournaments', [TournamentController::class, 'index']);
-//
-Route::get('/tournaments/create', [TournamentController::class, 'create']);
-//
-Route::get('/tournaments/{id}', [TournamentController::class, 'show']);
-//
-Route::post('/tournaments', [TournamentController::class, 'store']);
-//
-Route::get('/tournaments/{id}/edit', [TournamentController::class, 'edit']);
 
-Route::put('/tournaments/{id}', [TournamentController::class, 'update']);
-//
-Route::delete('/tournaments/{id}', [TournamentController::class, 'destroy']);
+// Route::get('/', function () {
+//     return view('welcome');
+// }); 
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+//
+Route::get('/tournaments', [TournamentController::class, 'index'])->middleware(['auth', 'verified'])->name('tournaments.index');
+//
+Route::get('/tournaments/create', [TournamentController::class, 'create'])->middleware(['auth', 'verified'])->name('tournaments.create');
+//
+Route::get('/tournaments/{id}', [TournamentController::class, 'show'])->name('tournaments.show');
+//
+Route::post('/tournaments', [TournamentController::class, 'store'])->middleware(['auth', 'verified'])->name('tournaments.store');
+//
+Route::get('/tournaments/{id}/edit', [TournamentController::class, 'edit'])->middleware(['auth', 'verified'])->name('tournaments.edit');
+//
+Route::put('/tournaments/{id}', [TournamentController::class, 'update'])->middleware(['auth', 'verified'])->name('tournaments.update');
+//
+Route::delete('/tournaments/{id}', [TournamentController::class, 'destroy'])->middleware(['auth', 'verified'])->name('tournaments.destroy');
+require __DIR__.'/auth.php';
